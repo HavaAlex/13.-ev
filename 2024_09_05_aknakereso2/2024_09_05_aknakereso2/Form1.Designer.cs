29e13a80b88f141d9d640d4cd0b161e28cbee99f
unknown <havasi.alex.marton@gmail.com>
2024-09-08 19:23:47 +0200

    13 ev feltoltve

---
+++ 2024_09_05_aknakereso2/2024_09_05_aknakereso2/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace _2024_09_05_aknakereso2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.magassag = new System.Windows.Forms.NumericUpDown();
+            this.szelesseg = new System.Windows.Forms.NumericUpDown();
+            this.mineNumber = new System.Windows.Forms.NumericUpDown();
+            this.Start = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Restart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.magassag)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szelesseg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mineNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(345, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(400, 400);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // magassag
+            // 
+            this.magassag.Location = new System.Drawing.Point(11, 68);
+            this.magassag.Name = "magassag";
+            this.magassag.Size = new System.Drawing.Size(120, 20);
+            this.magassag.TabIndex = 1;
+            this.magassag.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // szelesseg
+            // 
+            this.szelesseg.Location = new System.Drawing.Point(11, 94);
+            this.szelesseg.Name = "szelesseg";
+            this.szelesseg.Size = new System.Drawing.Size(120, 20);
+            this.szelesseg.TabIndex = 2;
+            this.szelesseg.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // mineNumber
+            // 
+            this.mineNumber.Location = new System.Drawing.Point(11, 120);
+            this.mineNumber.Name = "mineNumber";
+            this.mineNumber.Size = new System.Drawing.Size(120, 20);
+            this.mineNumber.TabIndex = 3;
+            this.mineNumber.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 39);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 7;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Könnyű",
+            "Közepes",
+            "Nehéz",
+            "Bosznia",
+            "Egyedi"});
+            this.comboBox1.Location = new System.Drawing.Point(11, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Nehézség";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Magassag";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Szélesség";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Aknaszám";
+            // 
+            // Restart
+            // 
+            this.Restart.Location = new System.Drawing.Point(11, 146);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(75, 23);
+            this.Restart.TabIndex = 10;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mineNumber);
+            this.Controls.Add(this.szelesseg);
+            this.Controls.Add(this.magassag);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Aknakereső (minesweeper úgymond)";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.magassag)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szelesseg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mineNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown magassag;
+        private System.Windows.Forms.NumericUpDown szelesseg;
+        private System.Windows.Forms.NumericUpDown mineNumber;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Restart;
+    }
+}
+
---
+++ 2024_09_05_aknakereso2/2024_09_05_aknakereso2/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2024_09_05_aknakereso2
+{
+    public partial class Form1 : Form
+    {
+        static List<List<bool>> bombak;
+        public Form1()
+        {
+            InitializeComponent();
+            alaphelyzet(comboBox1, magassag, szelesseg, mineNumber, label1, label2, label3, dataGridView1, Restart);
+            
+
+        }
+        static void alaphelyzet(ComboBox comboBox1, NumericUpDown magassag, NumericUpDown szelesseg, NumericUpDown mineNumber, System.Windows.Forms.Label label1, System.Windows.Forms.Label label2, System.Windows.Forms.Label label3, DataGridView dataGridView1, Button Restart)
+        {
+            comboBox1.SelectedIndex = 1;
+            customelrejt(magassag, szelesseg, mineNumber, label1, label2, label3);
+            magassag.Minimum = 5;
+            magassag.Maximum = 25;
+            magassag.Value = 10;
+            szelesseg.Minimum = 5;
+            szelesseg.Maximum = 25;
+            szelesseg.Value = 10;
+            mineNumber.Minimum = 1;
+            mineNumber.Maximum = (magassag.Value * szelesseg.Value) - 1;
+            mineNumber.Value = 10;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+            atmeretezes(dataGridView1);
+            bombak = new List<List<bool>>();
+            dataGridView1.MouseMove += egerjonmegy;
+            dataGridView1.Enabled = true;
+            Restart.Visible = false;
+            
+        }
+        
+        static void egerjonmegy(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+        }
+        static void customelrejt(NumericUpDown m, NumericUpDown sz, NumericUpDown db, System.Windows.Forms.Label elso, System.Windows.Forms.Label masodik, System.Windows.Forms.Label haromadik)
+        {
+            m.Visible = false;
+            sz.Visible = false;
+            db.Visible = false;
+            elso.Visible = false;
+            masodik.Visible = false;
+            haromadik.Visible = false;
+        }
+        static void custommegjelenik(NumericUpDown m, NumericUpDown sz, NumericUpDown db, System.Windows.Forms.Label elso, System.Windows.Forms.Label masodik, System.Windows.Forms.Label haromadik)
+        {
+            m.Visible = true;
+            sz.Visible = true;
+            db.Visible = true;
+            elso.Visible = true;
+            masodik.Visible = true;
+            haromadik.Visible = true;
+        }
+        static void atmeretezes(DataGridView t)
+        {
+            for (int i = 0; i < t.Columns.Count; i++)
+            {
+                t.Columns[i].Width = 40;
+            }
+            for (int i = 0;i < t.Rows.Count; i++)
+            {
+                t.Rows[i].Height = 40;
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if(mineNumber.Value > (magassag.Value * szelesseg.Value) - 1)
+            {
+                mineNumber.Value = ((magassag.Value * szelesseg.Value));
+            }
+            mineNumber.Maximum = (magassag.Value * szelesseg.Value) - 1;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            if (mineNumber.Value > (magassag.Value * szelesseg.Value) - 1)
+            {
+                mineNumber.Value = ((magassag.Value * szelesseg.Value));
+            }
+            mineNumber.Maximum = (magassag.Value * szelesseg.Value) - 1;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            mineNumber.Maximum = (magassag.Value * szelesseg.Value) - 1;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            bombak.Clear();
+            dataGridView1.RowCount = Convert.ToInt32(magassag.Value);
+            dataGridView1.ColumnCount = Convert.ToInt32(szelesseg.Value);
+            atmeretezes(dataGridView1);
+            palyacsinalas(magassag, szelesseg, mineNumber);
+            for (int i = 0; i < szelesseg.Value; i++)
+            {
+                for (int j = 0; j < magassag.Value; j++)
+                {
+                    dataGridView1.Rows[j].Cells[i].Value = "?";
+                }
+            }
+            Start.Visible = false;
+            Restart.Visible = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex != 4)
+            {
+                customelrejt(magassag, szelesseg, mineNumber, label1, label2, label3);
+            }
+            if (comboBox1.SelectedIndex == 0) 
+            {
+                magassag.Value = 10;
+                szelesseg.Value = 10;
+                mineNumber.Value = 5;
+                
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                magassag.Value = 10;
+                szelesseg.Value = 10;
+                mineNumber.Value = 10;
+               
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                magassag.Value = 10;
+                szelesseg.Value = 10;
+                mineNumber.Value = 25;
+                
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                magassag.Value = 10;
+                szelesseg.Value = 10;
+                mineNumber.Value = 75;
+               
+            }
+            else
+            {
+                custommegjelenik(magassag, szelesseg, mineNumber, label1, label2, label3);
+            }
+        }
+        static void palyacsinalas(NumericUpDown m, NumericUpDown sz, NumericUpDown db)
+        {
+            for (int i = 0; i < sz.Value; i++)
+            {
+                List<bool> sl = new List<bool>();
+                for (int j = 0; j < m.Value; j++)
+                {
+                    bool sb = false;
+                    sl.Add(sb);
+                }
+                bombak.Add(sl);
+            }
+            Random r = new Random();
+            aknak(Convert.ToInt32(m.Value), Convert.ToInt32(sz.Value), Convert.ToInt32(db.Value),r );
+        }
+        static void aknak(int m, int sz, int db, Random r)
+        {
+            
+            int k = db;
+            if (k == 0)
+            {
+
+            }
+            else
+            {
+                int x = r.Next(0, sz );
+                int y = r.Next(0, m );
+                if (!bombak[x][y])
+                {
+                    bombak[x][y] = true;
+                    k--;
+                    aknak(m, sz, k,r);
+                }
+                else 
+                { 
+                    aknak(m, sz, k, r);
+                }
+            }
+        }
+        public static bool bombavanmellete(int x, int y, out int találtTrueSzám)
+        {
+            
+            int[][] directions = new int[][]
+            {
+            new int[] {-1, -1}, 
+            new int[] {0, -1},  
+            new int[] {1, -1},  
+            new int[] {-1, 0},  
+            new int[] {1, 0},   
+            new int[] {-1, 1},  
+            new int[] {0, 1},   
+            new int[] {1, 1}    
+            };
+            int rows = bombak.Count;
+            int cols = bombak[0].Count;
+            int trueCount = 0;
+            foreach (var dir in directions)
+            {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+                if (newX >= 0 && newX < cols && newY >= 0 && newY < rows)
+                {
+                    if (bombak[newY][newX])
+                    {
+                        trueCount++;
+                    }
+                }
+            }
+            találtTrueSzám = trueCount;
+            return trueCount > 0;
+        }
+
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            wincheck(dataGridView1, Convert.ToInt32(mineNumber.Value));
+            int szomszedosc4 = 0;
+            if (!bombak[e.RowIndex][e.ColumnIndex] && bombavanmellete(e.ColumnIndex, e.RowIndex,out szomszedosc4))
+            {
+
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = Convert.ToString(szomszedosc4);
+            }
+            else if (!bombak[e.RowIndex][e.ColumnIndex])
+            {
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = "";
+            }
+            else
+            {
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = "bomba";
+                MessageBox.Show("Felrobbantál xd");
+                dataGridView1.Enabled = false;
+            }
+        }
+        static void wincheck(DataGridView dataGridView1, int db)
+        {
+            int maradekcellaszam = 0;
+            for (int i = 0; i < bombak.Count; i++){
+                for (int j = 0; j < bombak[i].Count; j++)
+                {
+                    if(dataGridView1.Rows[j].Cells[i].Value ==  "?")
+                    {
+                        maradekcellaszam++;
+                    }
+                }
+            }
+            if(maradekcellaszam-1 ==db)
+            {
+                dataGridView1.Enabled = false;
+                MessageBox.Show("Gratula győztél");
+            }
+        }
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            Start_Click(sender, e);
+            alaphelyzet(comboBox1, magassag, szelesseg, mineNumber, label1, label2, label3, dataGridView1, Restart);
+            Start.Visible = true;
+            
+        }
+    }
+}
